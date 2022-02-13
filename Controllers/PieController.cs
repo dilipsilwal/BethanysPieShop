457151024b12ff2471695fdc781d7273f054158a
silwal-spectre <dilipsilwal@gmail.com>
2022-02-13 01:39:20 -0800

    updated

---
+++ Controllers/PieController.cs
@@ -0,0 +1,37 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        
+
+        public ViewResult List()
+        {
+
+            //ViewBag.Message = "Test Message";
+            PiesListViewModel piesListViewModel = new()
+            { 
+                Pies = _pieRepository.AllPies,CurrentCategory="Cheese Cake"
+            };
+
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ Models/IPieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IPieRepository
+    {
+        List<Pie> AllPies { get; }
+        List<Pie> PiesOfTheWeek { get; }
+        Pie GetById(int pieId);
+    }
+}
